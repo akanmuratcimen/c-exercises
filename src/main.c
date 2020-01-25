bbf337bdc49604504ede6ac9d2c392540dde9a29
akanmuratcimen <akanmuratcimen@gmail.com>
2020-01-25 23:26:47 +0300

    queue_int

--- src/main.c
+++ src/main.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "queue_int.h"
-
 int main() {
-  queue_int_t* queue = queue_int_initialize(10);
-
-  queue_int_enqueue(queue, 1);
-  queue_int_enqueue(queue, 2);
-
-  queue_int_dequeue(queue);
-  queue_int_dequeue(queue);
-  queue_int_dequeue(queue);
-
-
-  
-
-  queue_int_clear(&queue);
-
   return EXIT_SUCCESS;
 }
